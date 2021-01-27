7a67825ec3ff22edd995785a5850045a0802f67b
Current-Value <current.value01@gmail.com>
2021-01-27 19:49:29 +0200

    context api

--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -4,20 +4,24 @@ import DialogItem from "./DialogItem/DialogsItem";
 import Message from "./Message/Message";
 import {sendMessageCreator, updateNewMessageBodyCreator} from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
+import StoreContext from "../../storeContext";
 
-const DialogsContainer = (props) => {
-    let state = props.store.getState().dialogsPage;
+const DialogsContainer = () => {
 
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator());
+    return <StoreContext.Consumer>
+        { store => {
+            let onSendMessageClick = () => {
+               store.dispatch(sendMessageCreator());
+            }
+            let onNewMessageChange = (body) => {
+                store.dispatch(updateNewMessageBodyCreator(body));
+            }
+            return <Dialogs updateNewMessageBody={onNewMessageChange}
+                            sendMessage={onSendMessageClick}
+                            dialogsPage={store.getState().dialogsPage}/>
+        }
     }
-
-    let onNewMessageChange = (body) => {
-        props.store.dispatch(updateNewMessageBodyCreator(body));
-    }
-
-    return <Dialogs updateNewMessageBody={onNewMessageChange} sendMessage={onSendMessageClick}
-                    dialogsPage={state}/>
+    </StoreContext.Consumer>
 }
 
 export default DialogsContainer;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "./storeContext";
 
 
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+                <Provider store={store}>
+                <App />
+                </Provider>
             </BrowserRouter>,
         </React.StrictMode>,
         document.getElementById('root')
@@ -21,11 +24,10 @@ let rerenderEntireTree = (state) => {
 
 
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
 store.subscribe( () => {
-    let state = store.getState();
-    rerenderEntireTree(state);
+    rerenderEntireTree();
 });
 
 // If you want to start measuring performance in your app, pass a function
