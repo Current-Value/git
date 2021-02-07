865bc2e7abe162ea9bb752a6b4b993fd4de71ee0
Current-Value <current.value01@gmail.com>
2021-02-07 19:13:27 +0200

    copy ProfileReduser

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,35 +1,33 @@
 import React from "react";
 import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
-import StoreContext from "../../../storeContext";
+import {connect} from "react-redux";
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
+import mapDispatchToProps from "react-redux/lib/connect/mapDispatchToProps";
 
 
 
 
-const MyPostsContainer = (props) => {
 
-    return (
-        <StoreContext.Consumer>
-            {
-            (store) => {
-                let state = store.getState();
-                let addPost = () => {
-                    store.dispatch(addPostActionCreator());
-                }
-
-                let onPostChange = (text) => {
-                    let action = updateNewPostTextActionCreator(text);
-                    store.dispatch(action);
-                }
-
-               return <MyPosts updateNewPostText={onPostChange}
-                         addPost={addPost}
-                         posts={store.getState().profilePage.posts}
-                         newPostText={store.getState().profilePage.newPostText}/>
-            }
-        }
-        </StoreContext.Consumer>
-    )
+const mapStateToProp  = (state) => {
+    return{
+        posts: state.profilePage.posts,
+        newPostText: state.profilePage.newPostText
+    }
 }
+
+const mapDispatchToProp = (dispatch) => {
+    return{
+        updateNewPostText: (text) => {
+            let action = updateNewPostTextActionCreator(text);
+            dispatch(action);
+        },
+        addPost: () => {
+            dispatch(addPostActionCreator());
+        }
+    }
+}
+
+const MyPostsContainer = connect(mapStateToProp, mapDispatchToProp)(MyPosts);
 
 export default MyPostsContainer;
--- src/index.js
+++ src/index.js
@@ -5,30 +5,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
-import {Provider} from "./storeContext";
+import {Provider} from "react-redux";
 
 
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>,
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store={store}>
-                <App />
-                </Provider>
-            </BrowserRouter>,
-        </React.StrictMode>,
-        document.getElementById('root')
-    );
-}
-
-
-
-rerenderEntireTree();
-
-store.subscribe( () => {
-    rerenderEntireTree();
-});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
