cb758f3a88b28d11eda4d63146228b2b7d3487f0
Current-Value <current.value01@gmail.com>
2021-04-11 19:08:43 +0300

    add to git

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import s from './Header.module.css'
 import {NavLink} from "react-router-dom";
+import logo from '../../assets/images/transparent-down-icon-price-icon-sale-icon-5db3fcdbb80b06.3632805615720767637538.jpg'
 
 const Header = (props) => {
     return <header className={s.header}>
-        <img src='https://www.freelogodesign.org/file/app/client/thumb/4ab55ad7-33c3-455c-90c7-c0305fc8eb9f_200x200.png?1609680675892' />
+        <img src={ logo } />
 
         <div className={s.loginBlock}>
             { props.isAuth
